d369b6b66c069223e43b5e302438f2e4aa9cdbd1
marcoscattolin <marco.scattolin@gmail.com>
2014-08-31 12:03:00 +0200

    new download data
    fetch dei dati dal sito della lega calcio

--- DownloadData.R
+++ DownloadData.R
@@ -1,6 +1,31 @@
 library(XML)
 
-url <- "http://www.corrieredellosport.it/calcio/serie_a/statistiche_classifiche_seriea.shtml"
-html <- htmlTreeParse(url)
-links <- getHTMLLinks(url)
-links <- links[grep("SerieA/statistiche",links)]
+url <- "http://www.legaseriea.it/it/"
+html <- htmlTreeParse(url,useInternalNodes=T)
+teamlinks <- xpathSApply(html,path="//div//dl[@class='serie_a']//dd//a",xmlGetAttr,"href")
+teams <- xpathSApply(html,path="//div//dl[@class='serie_a']//dd//a",xmlGetAttr,"title")
+teamlinks <- paste0("http://www.legaseriea.it",teamlinks)
+
+#### enter for loops
+for(i in 1:20){
+        team <- teams[i]
+        teamlink <- teamlinks[i]
+        html <- htmlTreeParse(teamlink,useInternalNodes=T)
+        players <- xpathSApply(html,path="//ul[@class='menu_tab']//li//a",xmlGetAttr,"href")[3]
+        players <- paste0("http://www.legaseriea.it/",players)
+        
+        html <- htmlTreeParse(players,useInternalNodes=T)
+        players <- readHTMLTable(players)[[1]]
+        colnames(players) <- c("numero","nome","datadinascita","ruolo","nazionalita","partitegiocate","reti","cartellinigialli","cartellinirossi")
+        
+        players <- players[-1,]
+        players$squadra <- team
+        
+        if (i == 1) {total <- players}
+        else{total <- rbind(total,players)}
+        
+        
+}
+
+total$squadra <- factor(total$squadra)
+
